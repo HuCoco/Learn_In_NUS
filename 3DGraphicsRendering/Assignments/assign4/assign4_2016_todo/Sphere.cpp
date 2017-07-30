1d7ce7675f77ad7ee3a37ba3c9ca7ec060a9f79d
Hu Ke <hu-coco@qq.com>
2017-07-30 23:12:50 +0800

    init and upload some materials of 3d graphics rendering in NUS

---
+++ 3DGraphicsRendering/Assignments/assign4/assign4_2016_todo/Sphere.cpp
@@ -0,0 +1,40 @@
+//============================================================
+// STUDENT NAME: 
+// NUS User ID.: 
+// COMMENTS TO GRADER: 
+// 
+// 
+// ============================================================
+//
+// FILE: Sphere.cpp
+
+
+
+#include <cmath>
+#include "Sphere.h"
+
+using namespace std;
+
+
+
+bool Sphere::hit( const Ray &r, double tmin, double tmax, SurfaceHitRecord &rec ) const 
+{
+    //***********************************************
+    //*********** WRITE YOUR CODE HERE **************
+    //***********************************************
+
+	return false; // You can remove/change this if needed.
+}
+
+
+
+
+bool Sphere::shadowHit( const Ray &r, double tmin, double tmax ) const 
+{
+    //***********************************************
+    //*********** WRITE YOUR CODE HERE **************
+    //***********************************************
+
+	return false; // You can remove/change this if needed.
+}
+
